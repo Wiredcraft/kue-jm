4878fa18253e69383a8d84861198d67ba925a158
chopperlee <lihengpro@gmail.com>
2016-12-09 18:05:56 +0800

    add JSON.parse for param data in jobs

--- test/fixture/task2.js
+++ test/fixture/task2.js
@@ -1,5 +1,5 @@
 module.exports = (param, previousResult, callback) => {
   setTimeout(() => {
-    callback(null, previousResult + JSON.parse(param).baz);
+    callback(null, previousResult + param.baz);
   }, 10);
 };
